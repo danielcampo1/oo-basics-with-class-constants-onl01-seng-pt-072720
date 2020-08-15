1c93022d7502a74c62f89759b97369950c4cef29
daniel campo <campo_daniel94@yahoo.com>
2020-08-15 14:36:49 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,6 +1,10 @@
+# learn spec/02_shoe_spec.rb
+
 class Shoe
   attr_accessor :color, :size, :material, :condition
-  attr_reader :brand
+  attr_reader :brand, :size
+  
+  BRANDS = []
 
   def initialize(brand)
     @brand = brand
@@ -10,5 +14,9 @@ class Shoe
     self.condition = "new"
     puts "Your shoe is as good as new!"
   end
-
+  
+  def size=(size)
+    @size = size
+    BRANDS << size
+  end
 end
